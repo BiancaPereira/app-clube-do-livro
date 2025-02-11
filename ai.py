697c2a3014ec5391b1929867a5f0c0ead2516d2d
Bianca Pereira <BiancaPereira@users.noreply.github.com>
2025-02-11 19:14:54 -0300

    refactor: refina o texto de geração

--- ai.py
+++ ai.py
@@ -7,7 +7,7 @@ client = genai.Client(api_key=gemini_key)
 
 def generate_ai_summary(book: str) -> str:
     response = client.models.generate_content(
-        model="gemini-2.0-flash", contents=f'Escreva um mini sinopse informal e chamativa sobre o livro {book} de até 2 linhas, adicionando o gênero no final entre parênteses e adiciona um emoji com uma bandeirinha do país de origem do autor.'
+        model="gemini-2.0-flash", contents=f'Escreva um mini sinopse chamativa sobre o livro {book} de até 1 linha, adicionando o gênero no final entre parênteses e adiciona o nome do autor.'
     )
 
     return response.text
